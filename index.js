1f4c059b765f3bf4e127a0874750d9e70e2b8311
fred <daktarispacetime@gmail.com>
2022-11-27 11:07:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+if (feet <= 400) return 'This one is on me!';
+if (feet <= 2000) return 'That will be twenty bucks.';
+if (2000 < feet && feet <= 2500) return 'I will gladly take your thirty bucks.'; 
+
+return 'No can do.';
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(City){
   // Write your code here!
+
+if(City === 'NYC') return "Ok, sounds good.";
+return "No go.";
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
+
+if(Tip === "generous") return 'Thank you so much.';
+if(Tip === "not as generous") return 'Thank you.';
+
+return "Bye.";
 }
